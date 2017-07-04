9b5400cb5be9b45a2ea8b8a4d8e9cd6636261f35
Emlyn Bolton-Maggs <emlyn.bolton@momentussoftware.com>
2017-07-04 13:30:35 -0700

    Redux testing complete

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
 
 	constructor(props) {
 		super(props);
-
 		this.state = { comment: "" };
 	}
 
@@ -14,6 +15,7 @@ export default class CommentBox extends Component {
 
 	handleSubmit(event) {
 		event.preventDefault();
+		this.props.saveComment(this.state.comment);
 		this.setState({ comment: "" })
 	}
 
@@ -21,11 +23,14 @@ export default class CommentBox extends Component {
 		return (
 			<form onSubmit={this.handleSubmit.bind(this)}
 				  className="comment-box">
+				<h4>Add a comment</h4>
 				<textarea value={this.state.comment} 
 						  onChange={this.handleChange.bind(this)}
 				/>
-				<button>Submit</button>
+				<div><button>Submit</button></div>
 			</form>
 			)
 	}
 }
+
+export default connect(null, actions)(CommentBox);
